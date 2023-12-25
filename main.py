3401001fe2cf804020e375a253f633a63bfff2d4
sqrtleet <sqrtleet@gmail.com>
2023-12-26 02:16:08 +0900

    fix uvicorn

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def get_excel_count(data: dict):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
